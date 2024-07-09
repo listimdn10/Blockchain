264f445dae67e77c6185437cdc49065881ddade0
listimdn10 <quynhnhu170218@gmail.com>
2024-07-09 21:05:42 +0700

    mocha, providers, web3

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -3,20 +3,20 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');//constructor
 const web = new Web3(ganache.provider());//instance 
 
-//get to know Mocha
-class Car {
-    park(){
-        return 'stopped';
-    }
-    drive(){
-        return 'vroom';
-    }
-}
+// //get to know Mocha which is to run test 
+// class Car {
+//     park(){
+//         return 'stopped';
+//     }
+//     drive(){
+//         return 'vroom';
+//     }
+// }
 
 
-describe('Car', ()=> {
-    it('can park', () => {
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    });
-});
+// describe('Car', ()=> {
+//     it('can park', () => {
+//         const car = new Car();
+//         assert.equal(car.park(), 'stopped');
+//     });
+// });
