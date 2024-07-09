4dce1a8c09dbb226f05c61cb44be1ab3434c8148
listimdn10 <quynhnhu170218@gmail.com>
2024-07-09 21:46:08 +0700

    async await instead of then

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache');
 const { Web3 } = require('web3');//constructor
-const web = new Web3(ganache.provider());//instance 
+const web3 = new Web3(ganache.provider());//instance 
 
 // //get to know Mocha which is to run test 
 // class Car {
@@ -22,21 +22,22 @@ const web = new Web3(ganache.provider());//instance
 // });
 
 
+let accounts;
+
 //fetching accounts from ganache network 
 //account from ganache is unlocked 
 
-beforeEach ( () => {
+beforeEach (async () => {
     //GET A LIST OF ALL ACCOUNT 
-    web3.eth.getAccounts() //use eth modules of the web3, use the getaccount of eth 
-        .then(fetchedAccounts=> {
-            console.log(fetchedAccounts);
-        })
+    //try async and await to more readable code
+    accounts = await web3.eth.getAccounts() //use eth modules of the web3, use the getaccount of eth 
+       
     //Use one ò those account
     //the contract
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        
+        console.log(accounts);
     })
 })
